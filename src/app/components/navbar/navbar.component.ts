9dabc0b9c19ce848c5171e80d8f66bbfb9c00d97
Mike <mich@elwarner.co.uk>
2018-11-06 04:17:00 +0000

    Added Google Authentication to Navbar Added Party Admin Page

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-navbar',
@@ -7,11 +10,21 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  @Input() brand: string;
-
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth,
+              public db: AngularFirestore) {}
 
   ngOnInit() {
+    this.afAuth.user.subscribe(user => {
+      this.db.collection('users').doc(user.email).set(user.toJSON());
+    });
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
--- src/app/components/party/party.component.ts
+++ src/app/components/party/party.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+class Party {
+  id: number;
+  name: string;
+  description: string;
+  constructor() {
+    this.id = 0;
+    this.name = '';
+    this.description = '';
+  }
+}
 
 @Component({
   selector: 'app-party',
@@ -7,9 +19,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PartyComponent implements OnInit {
 
-  constructor() { }
+  public parties: any;
+  public newParty: Party;
+
+  constructor(public db: AngularFirestore) {
+    db.collection('parties').valueChanges().subscribe(
+      value => {
+        // Set parties from firebase db
+        this.parties = value;
+        // Sort parties by id
+        this.parties.sort((a, b) => {
+          const x = a.id;
+          const y = b.id;
+          return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
+        // console.log(this.parties);
+      }
+    );
+  }
 
   ngOnInit() {
+    this.newParty = new Party();
+  }
+
+  addParty(): void {
+    this.newParty.id = this.parties.length;
+    this.db.collection('parties').doc('party' + this.newParty.id).set(JSON.parse(JSON.stringify(this.newParty)));
+  }
+
+  editParty(id: number): void {
+    let docId: string;
+    this.db.collection('parties').get().subscribe(partyDoc => {
+      partyDoc.forEach(doc => {
+        if (parseInt(doc.id.substr(-1), 10) === parseInt(id.toString(), 10)) {
+          console.log(doc.id);
+          docId = doc.id;
+        }
+      });
+    }, err => {
+      console.error(err);
+    }, () => {
+      this.db.collection('parties').doc(docId).valueChanges().subscribe(partyDoc => {
+        console.log('partyDoc: ', partyDoc);
+      });
+    });
+  }
+
+  deleteParty(id: number): void {
+    this.db.collection('parties').get().subscribe(partyDoc => {
+      partyDoc.forEach(doc => {
+        if (parseInt(doc.id.substr(-1), 10) === parseInt(id.toString(), 10)) {
+          this.db.collection('parties').doc(doc.id).delete();
+        }
+      });
+    });
   }
 
 }
